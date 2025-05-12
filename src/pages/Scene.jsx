a968807696b5972601b82e7a865952f86543a401
reynaldo0 <reynaldooyusellino@gmail.com>
2025-05-12 23:23:30 +0700

    upd : set up scene three

---
+++ src/pages/Scene.jsx
@@ -0,0 +1,48 @@
+import { ScrollControls } from "@react-three/drei";
+import { Canvas } from "@react-three/fiber";
+import { EffectComposer, Noise } from "@react-three/postprocessing";
+import { useMemo } from "react";
+import { usePlay } from "../context/play";
+import { Overlay } from "../components/models/components/Overlay";
+import { Experience } from "../components/models/components/Experience";
+
+function App() {
+  const { play, end } = usePlay();
+
+  const effects = useMemo(
+    () => (
+      <EffectComposer>
+        <Noise opacity={0.08} />
+      </EffectComposer>
+    ),
+    []
+  );
+
+  return (
+    <>
+      <Canvas>
+        <color attach="background" args={["#ececec"]} />
+        <ScrollControls
+          pages={play && !end ? 20 : 0}
+          damping={0.5}
+          style={{
+            top: "10px",
+            left: "0px",
+            bottom: "10px",
+            right: "10px",
+            width: "auto",
+            height: "auto",
+            animation: "fadeIn 2.4s ease-in-out 1.2s forwards",
+            opacity: 0,
+          }}
+        >
+          <Experience />
+        </ScrollControls>
+        {effects}
+      </Canvas>
+      <Overlay />
+    </>
+  );
+}
+
+export default App;
