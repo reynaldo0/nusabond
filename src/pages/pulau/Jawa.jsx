da9cc30d40b6b0d79da20b274f2b0de446999d9c
reynaldo0 <reynaldooyusellino@gmail.com>
2025-05-18 21:18:53 +0700

    upd

--- src/pages/pulau/Jawa.jsx
+++ src/pages/pulau/Jawa.jsx
@@ -1,10 +1,14 @@
 import React from "react";
 import Hero from "./templates/Hero";
+import Kebudayaan from "./templates/Kebudayaan";
+import Content from "./templates/Content";
 
 const Jawa = () => {
   return (
     <>
       <Hero />
+      <Kebudayaan />
+      <Content />
     </>
   );
 };
