48884e2094209eb4b67b1af644bbdd4e7717bc69
reynaldo0 <reynaldooyusellino@gmail.com>
2025-06-08 16:01:27 +0700

    upd image

--- src/docs/jawaBudaya.js
+++ src/docs/jawaBudaya.js
@@ -9,34 +9,34 @@ export const jawaBudaya = [
   },
   {
     title: "Gamelan (Suku Jawa)",
-    image: "/budaya/jawa/Gamelan.png",
+    image: "/budaya/jawa/gamelan.png",
     desc: "Bicara soal musik tradisional, gamelan punya pesona yang nggak bisa ditolak. Suara gong, kendang, dan saron berpadu harmonis menciptakan alunan yang menenangkan jiwa. Bondmates, serunya lagi, gamelan nggak sekadar musik. Ini adalah simbol dari filosofi Jawa yang menjunjung harmoni dalam hidup. Kalau lagi dengerin gamelan, apa yang kalian rasain? Damai? Tenang? Atau malah pengen langsung belajar main salah satu alatnya?",
     url:
       "https://digilib.isi.ac.id/5465/1/Gamelan%20Jawa%2C%20Seni%20Musik.PDF",
   },
   {
     title: "Jaipongan (Suku Sunda)",
-    image: "/budaya/jawa/Jaipongan.png",
+    image: "/budaya/jawa/jaipongan.png",
     desc: "Bondmates, pernah nggak kalian nonton Jaipongan? Tarian ini punya energi yang luar biasa. Gerakannya dinamis, menggambarkan keceriaan sekaligus keberanian perempuan Sunda. Bayangin, penari dengan selendang warna-warni yang melambai mengikuti irama gendang, gerakannya tegas tapi tetap elegan. Bondmates tertarik untuk belajar Jaipongan, nggak nih?",
     url:
       "https://journal.isi.ac.id/index.php/resital/article/viewFile/394/33.pdf",
   },
   {
     title: "Wayang Golek (Suku Sunda)",
-    image: "/budaya/jawa/Wayang.png",
+    image: "/budaya/jawa/wayang.png",
     desc: "Wayang golek bukan cuma sekadar hiburan, loh, Bondmates. Ini adalah seni pertunjukan yang penuh pesan kehidupan. Setiap tokoh wayang punya cerita yang mendalam, ada yang jadi simbol kebaikan, ada juga yang mencerminkan sisi gelap manusia. Menonton wayang golek itu seperti membaca buku filsafat, tapi versi visual. Kita diajak merenungkan mana yang benar dan salah, serta bagaimana menjalani hidup dengan bijak. Apa Bondmates pernah punya tokoh wayang favorit? Kalau belum, cobain deh tonton pertunjukannya!",
     url:
       "https://drive.google.com/file/d/1hX-t36KEl531fVPRJsnW5JYlagN7WBCC/preview",
   },
   {
     title: "Palang Pintu (Suku Betawi)",
-    image: "/budaya/jawa/Palang.png",
+    image: "/budaya/jawa/palang.png",
     desc: "Tradisi Palang Pintu adalah salah satu tradisi pernikahan unik di Indonesia, Bondmates! Di sini, calon pengantin pria harus membuktikan dirinya pantas menikahi sang pujaan hati. Caranya? Melewati berbagai ujian seperti berbalas pantun hingga adu silat. Lucunya, ini semua dilakukan dengan suasana yang ceria dan penuh tawa. Ujian ini nggak cuma sekadar simbol, tapi juga menunjukkan kesungguhan calon pengantin pria. Seru banget, ya!",
     url: "https://repository.uniga.ac.id/file/mahasiswa/1124893983.pdf",
   },
   {
     title: "Lenong (Suku Betawi)",
-    image: "/budaya/jawa/Lenong.png",
+    image: "/budaya/jawa/lenong.png",
     desc: "Lenong adalah teater khas Betawi yang penuh kelucuan dan kritik sosial. Dengan bahasa Betawi yang kocak, lenong sering mengangkat isu-isu kehidupan sehari-hari yang relevan. Yang seru, lenong selalu dibarengi musik tradisional seperti gambang kromong, bikin suasananya makin hidup!",
     url:
       "https://media.neliti.com/media/publications/476249-none-570764d3.pdf",
